5d0ac07abcf5f0e8e428f8cc79b65e5b9be75381
GontaGantiPC <alfinohasan629@gmail.com>
2025-03-15 16:36:44 +0700

    Add Activity Register and set layout component

---
+++ app/src/main/java/com/example/manajemenreportfinansialumkm/ui/register/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.example.manajemenreportfinansialumkm.ui.register
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.manajemenreportfinansialumkm.R
+import com.example.manajemenreportfinansialumkm.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
